c40eedb190360c232a1144164f91c697640fd47d
bertil291utn <bertilt4@gmail.com>
2019-08-27 23:56:49 -0500

    Instrucciones y detalles  estacionamiento

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.js';
 
 import { Geolocation } from '@ionic-native/geolocation/ngx';
-import { LoadingController } from '@ionic/angular';
+import { LoadingController, ToastController } from '@ionic/angular';
 import { TitleCasePipe } from '@angular/common';
 
 @Component({
@@ -13,12 +13,19 @@ import { TitleCasePipe } from '@angular/common';
 })
 export class Tab2Page {
   accessToken: string = 'pk.eyJ1IjoiYmFydG8zODAiLCJhIjoiY2pzdjlub2NkMDRzdDN5bnp2N3Y0dnVmeCJ9.-9pWtrRt5xe5vVfXu6rQ3A';
-  loadingMap;
-  pinEnable: boolean = false;
-  propiedadesObject;
+  loadingMap;//loading controller mapa
+  loadingParkingLayer;//loading layer cuand cancela o reserva una ruta ara qu vuelva a cargar
+  footerEnable: boolean = false;
+  propiedadesObject;//para los datos del footer
+  map;//el mapa
+  destinoParking;//destino para la creacion de la ruta 
+  reservado: boolean = false;//para ver si esta reservado
+  markerParking;//marcador para mostrar cuando hace click en un parking 
+  dirEnable: boolean = true;//activar o desactivar el view de la direccion, inicia mostrando
+  directions;//var para la direciones y rutas
 
   constructor(private geolocation: Geolocation, public loadingController: LoadingController, private titlecasePipe: TitleCasePipe
-  ) { }
+    , public toastController: ToastController) { }
 
   ngOnInit() {
 
@@ -35,7 +42,7 @@ export class Tab2Page {
     let center = { lon: myLatLng.lng, lat: myLatLng.lat };
 
     mapboxgl.accessToken = this.accessToken;
-    let map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: 'map_canvas',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: center, //-78.1571629,-0.0508833 // { lon: -78.1523295,lat:0.0388447 } -78.1523295,lat:0.0388447
@@ -46,79 +53,170 @@ export class Tab2Page {
     // .setLngLat(center)
     // .addTo(map);
 
-    map.addControl(new mapboxgl.GeolocateControl({
+    this.map.addControl(new mapboxgl.GeolocateControl({
       positionOptions: {
         enableHighAccuracy: true
       },
       trackUserLocation: true
     }));//anadir boton de ubicacion punto azul
 
-    // var directions = new MapboxDirections({
-    //   accessToken: this.accessToken,
-    //   unit: 'metric',
-    //   profile: 'mapbox/driving-traffic',
-    //   congestion: true,
-    //   controls: { inputs: false },
-    //   language: 'es'
-    // });
-    // directions.setOrigin([center.lon, center.lat]);
-
-    // map.addControl(directions, 'top-left');
-
-
-
-    map.on('load', () => {
+    this.map.on('load', () => {
       this.asignarObjetoGeoJson();
-      this.anadirLayerParking(map);
+      this.anadirLayerParking();
       this.loadingMap.dismiss();
     });//
 
-
-    this.viewFooter(map);//mostrar footer con valores
+    this.viewFooter();//mostrar footer con valores
 
   }//initmapbox
 
+  public async estoyEstacionado() {
+    this.loadingParkingLayer = await this.loadingController.create({
+      message: 'Map loading...'
+    });
+    await this.loadingParkingLayer.present();
+    this.anadirLayerParking();
+    this.directions.removeRoutes();//remover las rutas
+    document.getElementsByClassName("directions-control-instructions").item(0).remove();// remover as insutrcciones
+    this.reservado = false//cambiar el estado de reservado
+    this.footerEnable = false;//y deshabiliatr el footer
+    //poner como reservado durante un minuto en la BD , si en ese tiempo el de la zona azul no pone como estacionado el puesto se libera 
+    //pasado el un minuto comprobar si hay comprobacion por parte del senor de la zona azul 
+    //cuando el zona azul pone estacionado directamente no hay necesidad de comprobacion 
+    if (this.map.getLayer('parking'))
+      if (this.loadingParkingLayer != null || undefined) {
+        this.loadingParkingLayer.dismiss();
+        console.log('usted se ha estacionado');
+        let mensaje = 'Usted se enceuntra estacionado en el parking No. ' + this.propiedadesObject.id;
+        this.actionPresentToast(mensaje, 2000);
+      }
+  }
+
+  public async cancelarParking() {
+    //para cancelar una ruta debe volver a anadir la fuente de datos y  la capa de los parkings (el dibujo)
+    this.loadingParkingLayer = await this.loadingController.create({
+      message: 'Map loading...'
+    });
+    await this.loadingParkingLayer.present();
+    this.anadirLayerParking();
+    this.directions.removeRoutes();//remover las rutas
+    document.getElementsByClassName("directions-control-instructions").item(0).remove();// remover as insutrcciones
+    // this.map.removeControl(this.directions);//remover la capa de controles
+    this.reservado = false//cambiar el estado de reservado
+    this.footerEnable = false;//y deshabiliatr el footer
+    if (this.map.getLayer('parking'))
+      if (this.loadingParkingLayer != null || undefined) {
+        this.loadingParkingLayer.dismiss();
+      }
+  }
 
 
-  private viewFooter(map) {
+  public async gotoReservar() {
+    let loadingRuta = await this.loadingController.create({
+      message: 'Trazando ruta...'
+    });
+    await loadingRuta.present();
 
-    let markerParking = new mapboxgl.Marker();
-    map.on('click', () => {
-      if (this.pinEnable) {
-        markerParking.remove();
-        this.pinEnable = false;
+    this.reservado = true;//cambiar el estado a reervado
+    //remover la fuente de datos de parking
+    //remover la capa de dibujado de parkings
+    this.map.removeLayer('parking');
+    this.map.removeSource('tilequery')
+    this.markerParking.remove();//remover el marker 
+    const myLatLng = await this.getLocation();
+    let center = { lon: myLatLng.lng, lat: myLatLng.lat };
+    this.directions = new MapboxDirections({
+      accessToken: this.accessToken,
+      unit: 'metric',
+      profile: 'mapbox/driving-traffic',
+      congestion: true,
+      controls: { inputs: false },//desahbiliatr los controles de trafifc, bike, driving, etc
+      language: 'es',
+      interactive: false //que no se pueda manipular
+    });
+    this.directions.setOrigin([center.lon, center.lat]);
+    this.directions.setDestination(this.destinoParking);
+    this.map.addControl(this.directions, 'top-left');//anadir en el mapa
+    this.directions.on('route', () => {
+      loadingRuta.dismiss();
+    });
+    //deshabilitar el objeto de las  direcciones (el cuadro flotante negro)
+    let instructions = document.getElementsByClassName("directions-control-instructions");
+    instructions[0]['hidden'] = true;
+  }
+
+  public showHideInstruccionsDir() {
+    //mostra u ocultar el objeto de direcicones 
+    let instructions = document.getElementsByClassName("directions-control-instructions");
+    if (instructions[0]['hidden'])
+      instructions[0]['hidden'] = false;
+    else
+      instructions[0]['hidden'] = true;
+  }
+
+  public showHideDir() {
+    //mostrar u ocultar el div de direcciones en la aprte de abajo
+    if (this.dirEnable)
+      this.dirEnable = false;
+    else
+      this.dirEnable = true;
+  }
+
+  private viewFooter() {
+    //ver las drieecciones una vez que presiona en un parking especifico 
+    this.markerParking = new mapboxgl.Marker();
+    this.map.on('click', () => {
+      if (this.footerEnable && !this.reservado) {
+        this.markerParking.remove();
+        this.footerEnable = false;
       }
     });//verificar si hace click en el map y si ha dado click a cualquier de los estacionamientos, se  va a quitar el foot y el marker
 
-
-    map.on('click', 'parking', (e) => {
-      // var title = '<h3><strong>No. </strong>' + e.features[0].properties.id + '</h3>';
-      // var storeAddress = '<p><strong>Tipo. </strong>' + this.titlecaseTransform(e.features[0].properties.tipo) + '</p>';
+    //cuando presiona en el layer del parking 
+    //enviar val propiedadesobject para mostra en el footer y anadir el marker
+    this.map.on('click', 'parking', (e) => {
       var coordinates = e.features[0].geometry.coordinates.slice();
-      // var content = title + storeAddress;
-
-      // popup.setLngLat(coordinates)
-      //   .setHTML(content)
-      //   .addTo(map);
+      this.destinoParking = coordinates;
       this.propiedadesObject = e.features[0].properties;
-      // console.log('e.features[0].propiedades: ', e.features[0].propiedades);
-      markerParking.setLngLat(coordinates)
-        .addTo(map);
-      this.pinEnable = true;
+      this.markerParking.setLngLat(coordinates)
+        .addTo(this.map);
+      this.footerEnable = true;
     });
+
+
+
+    // this.map.on('click', 'parking', (e) => {
+    //   // var title = '<h3><strong>No. </strong>' + e.features[0].properties.id + '</h3>';
+    //   // var storeAddress = '<p><strong>Tipo. </strong>' + this.titlecaseTransform(e.features[0].properties.tipo) + '</p>';
+    //   var coordinates = e.features[0].geometry.coordinates.slice();
+    //   this.destinoParking = coordinates;
+    //   // var content = title + storeAddress;
+
+    //   // popup.setLngLat(coordinates)
+    //   //   .setHTML(content)
+    //   //   .addTo(map);
+    //   this.propiedadesObject = e.features[0].properties;
+    //   // console.log('e.features[0].propiedades: ', e.features[0].propiedades);
+    //   this.markerParking.setLngLat(coordinates)
+    //     .addTo(this.map);
+    //   this.footerEnable = true;
+    // });
   }
 
-  private anadirLayerParking(map) {
-    map.addSource('tilequery', { //crear una fuente de tipo geojson ,de nombre tilequery
+  private anadirLayerParking() {
+    this.map.addSource('tilequery', { //crear una fuente de tipo geojson ,de nombre tilequery
       type: "geojson",
       data: this.geojson
     });
+    this.addLayer();//crear el layer en el mapa
+  }
 
-    map.addLayer({ //anadir el layer con el dataset tilequery
+  private addLayer() {
+    this.map.addLayer({
       id: "parking",
       type: "circle",
-      source: "tilequery", // Set the layer source
-      paint: {//anadir esilo al layer 
+      source: "tilequery",
+      paint: {
         "circle-stroke-color": "white",
         "circle-stroke-width": {
           stops: [
@@ -134,15 +232,20 @@ export class Tab2Page {
           ],
           base: 5
         },
-        "circle-color": [ // Specify the color each circle should be
-          'match', // Use the 'match' expression: https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
-          ['get', 'tipo'], // Use the result 'tipo' property
+        "circle-color": [
+          'match',
+          ['get', 'tipo'],
           'automovil', '#FF8C00',
           'motocicleta', '#9ACD32',
           'especial', '#008000',
-          '#FF0000']/*  default value */
+          '#FF0000'
+        ]
       }
     });
+    //cuando el loadin parking se haya activado
+    // if (this.map.getLayer('parking'))
+    //   if (this.loadingParkingLayer != null || undefined)
+    //     this.loadingParkingLayer.dismiss();
   }
 
   private async getLocation() {
@@ -179,7 +282,6 @@ export class Tab2Page {
       featuresArray.push(features);//add a un array
     });
     this.geojson.features = featuresArray;//anadir el array de features en el campo fetures del objeto geojson
-    console.log('this.geojson: ', this.geojson);
 
   }
 
@@ -195,4 +297,13 @@ export class Tab2Page {
   }
 
 
+
+  private async actionPresentToast(mensaje: string, duration: number) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      position: 'top',
+      duration: duration
+    });
+    toast.present();
+  }
 }//end class
